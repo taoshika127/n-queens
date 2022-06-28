91e3c9f3fa19e6cb19454cc8861d48c161d48904
Ken Kurita <kenkurita@Kens-MacBook-Pro.local>
2022-06-27 20:50:38 -0700

    Diagnals - incomplete

--- src/Board.js
+++ src/Board.js
@@ -5,12 +5,12 @@
 (function() {
 
   window.Board = Backbone.Model.extend({
-
+// commented out logs. need to remove later
     initialize: function (params) {
       if (_.isUndefined(params) || _.isNull(params)) {
-        console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
-        console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
-        console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
+       // console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
+        //console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
+       // console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
       } else if (params.hasOwnProperty('n')) {
         this.set(makeEmptyMatrix(this.get('n')));
       } else {
@@ -42,6 +42,7 @@
     },
 
     hasAnyQueenConflictsOn: function(rowIndex, colIndex) {
+      //console.log(this);
       return (
         this.hasRowConflictAt(rowIndex) ||
         this.hasColConflictAt(colIndex) ||
@@ -79,12 +80,34 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+
+      // for key in attributes
+      if (!rowIndex && rowIndex !== 0) {
+        return false;
+      }
+
+      var arrAtRowIndex = this.attributes[rowIndex];
+      //console.log(arrAtRowIndex, this);
+      var totalNumber = arrAtRowIndex.reduce((accum, currValue) => accum + currValue, 0);
+      //console.log(totalNumber);
+      if (totalNumber <= 1) {
+        return false;
+      } else {
+        return true;
+      }
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var boardObj = this.attributes;
+      for(var rowIndex in boardObj) {
+        rowIndex = Number(rowIndex);
+        // console.log(this.hasRowConflictAt(rowIn))
+         if(this.hasRowConflictAt(rowIndex)) {
+           return true;
+         }
+      }
+      return false;
     },
 
 
@@ -94,12 +117,33 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      if (!colIndex && colIndex !== 0) {
+        return false;
+      }
+      var colArr = [];
+      for (var key in this.attributes) {
+        colArr.push(this.attributes[key][colIndex])
+      }
+      colArr.pop();
+      //console.log(colArr);
+
+      var result = colArr.reduce((accum, currValue) => accum + currValue, 0);
+      return result > 1;
+
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var boardObj = this.attributes;
+      for(var colIndex in boardObj) {
+        colIndex = Number(colIndex);
+        // console.log(this.hasRowConflictAt(rowIn))
+         if(this.hasColConflictAt(colIndex)) {
+           console.log(colIndex)
+           return true;
+         }
+      }
+      return false;
     },
 
 
@@ -109,6 +153,10 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+
+      console.log(majorDiagonalColumnIndexAtFirstRow)
+
+
       return false; // fixme
     },
 
--- src/solvers.js
+++ src/solvers.js
@@ -14,11 +14,11 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
+// commented out the consoles.. need to add back in later
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
@@ -26,7 +26,7 @@ window.findNRooksSolution = function(n) {
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  //console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
@@ -34,7 +34,7 @@ window.countNRooksSolutions = function(n) {
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  //console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
@@ -42,6 +42,6 @@ window.findNQueensSolution = function(n) {
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  //console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
